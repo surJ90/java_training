196d064f5aa538c3940dd1f3debab300fa30b78a
surJ90 <suraj.patil.4990@gmail.com>
2025-05-05 14:05:00 +0530

    assignment four solutions

---
+++ src/HasAIsA.java
@@ -0,0 +1,13 @@
+class Animal {}
+class Dog extends Animal {} // Dog Is-A Animal (inheritance)
+
+class Engine {}
+class Car {
+    Engine v12 = new Engine(); // Car Has-A Engine (composition)
+}
+
+public class HasAIsA {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/OverridingDemo.java
@@ -0,0 +1,63 @@
+class Parent {
+    // 1. Access modifier
+    protected void display() {
+        System.out.println("Parent display");
+    }
+    // 2. Final method
+    final void finalMethod() {
+        System.out.println("Final method");
+    }
+    // 3. Static method
+    static void staticMethod() {
+        System.out.println("Static method");
+    }
+    // 4. Private method
+    private void privateMethod() {
+        System.out.println("Private method");
+    }
+    // 5. Covarient return type
+    Parent getObject() {
+        return this;
+    }
+    // 6. Super call & 7. Exception handling
+    void process() throws RuntimeException {
+        System.out.println("Parent process");
+    }
+}
+
+class Child extends Parent {
+    @Override 
+    public void display() { // Changing access modifier from protected to public
+        System.out.println("Child display");
+    }
+    // Final method cannot be overridden
+    // void finalMethod() {
+    //     System.out.println("Final method");
+    // } -> Compilation error
+    static void staticMethod() { // static method cannot be overridden, can be hidden
+        System.out.println("Child static");
+    }
+    void privateMethod() { // not inherited, not overridden
+        System.out.println("Child private");
+    }
+    @Override
+    Child getObject() { // Covarient return type
+        return this;
+    }
+    @Override // Invoking parent's override method
+    void process() { // same or narrower exception handling
+        super.process(); // calls parent's method
+        System.out.println("Child process");
+    }
+}
+
+public class OverridingDemo {
+    public static void main(String[] args) {
+        Parent parent = new Child();
+        parent.display();
+        parent.process();
+        System.out.println(parent.getObject().getClass());
+        Parent.staticMethod();
+        Child.staticMethod();
+    }
+}
